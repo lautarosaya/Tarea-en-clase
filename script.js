9c8048320ef657137ac4a400afd5f947498737d3
lautarosaya <lautarosaya0012@gmail.com>
2022-07-21 21:18:55 -0300

    tarea clase 21/7

---
+++ script.js
@@ -0,0 +1,10 @@
+function suma_y_divi(){
+    numUno = prompt("ingresa primer numero: ");
+    numDos = prompt("ingresa segundo numero: ");
+
+    rtaResta = numUno - numDos;
+    rtaDivi = numUno / numDos;
+    document.write(rtaResta+"<br>"+rtaDivi);
+}
+
+
